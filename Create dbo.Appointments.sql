db1ad4053af64a10bc16cd76fce28bd60e76654c
Vincent Grice <39641488+vgrice888@users.noreply.github.com>
2019-03-03 20:38:26 -0500

    Finished appointment model and able to schedule appointment from customer info

---
+++ Create dbo.Appointments.sql
@@ -0,0 +1,20 @@
+﻿USE [RealEstate]
+GO
+
+/****** Object: Table [dbo].[Appointments] Script Date: 2/26/2019 2:48:24 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Appointments] (
+    [Id]                  INT            IDENTITY (1, 1) NOT NULL,
+    [CustomerName]        NVARCHAR (MAX) NULL,
+    [CustomerEmail]       NVARCHAR (MAX) NULL,
+    [CustomerPhoneNumber] NVARCHAR (MAX) NULL,
+    [AppointmentDate]     DATETIME2 (7)  NOT NULL,
+	[Discriminator]		  INT			 NULL
+);
+
+
